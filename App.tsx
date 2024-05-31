58f6e7af762051ff58eec95cdbadd1adb8d63866
Banda-Aakash <aakashbanda03@gmail.com>
2024-05-31 14:19:41 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { enableScreens } from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {enableScreens} from 'react-native-screens';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
-import { Vibration, View } from 'react-native';
+import {Vibration, View} from 'react-native';
 import SmsReq from './components/SmsReq';
 import SplashScreen from './components/SplashScreen';
 
@@ -14,23 +14,25 @@ const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen
-    //       name="Home"
-    //       component={LoginPage}
-    //       options={{ headerShown: false }} // Hide header for LoginPage
-    //     />
-    //     <Stack.Screen
-    //       name="SignUp"
-    //       component={SignUpPage}
-    //       options={{ headerShown: false }} // Hide header for SignUpPage
-    //     />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
     <>
-    <SplashScreen/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={LoginPage}
+            options={{headerShown: false}} // Hide header for LoginPage
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpPage}
+            options={{headerShown: false}} // Hide header for SignUpPage
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
+    // <>
+    // <SplashScreen/>
+    // </>
   );
 };
 
