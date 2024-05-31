a3516b38edf99fe8fe63b8345863e233f71d67ca
Banda-Aakash <aakashbanda03@gmail.com>
2024-05-31 14:43:22 +0530

    Splash Screen Done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {enableScreens} from 'react-native-screens';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
-import {Vibration, View} from 'react-native';
+import {Vibration, View, useAnimatedValue} from 'react-native';
 import SmsReq from './components/SmsReq';
 import SplashScreen from './components/SplashScreen';
 
@@ -13,22 +13,32 @@ enableScreens();
 const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
+  const [isSplash, setisSplash] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setisSplash(false);
+    },3000);
+  });
+  
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={LoginPage}
-            options={{headerShown: false}} // Hide header for LoginPage
-          />
-          <Stack.Screen
-            name="SignUp"
-            component={SignUpPage}
-            options={{headerShown: false}} // Hide header for SignUpPage
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      {
+        isSplash?<SplashScreen/>:
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={LoginPage}
+              options={{headerShown: false}} // Hide header for LoginPage
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUpPage}
+              options={{headerShown: false}} // Hide header for SignUpPage
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      }
     </>
     // <>
     // <SplashScreen/>
